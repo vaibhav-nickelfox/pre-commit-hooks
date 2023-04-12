e78fb85bc3b7e3971f034a6abca2908b72080640
Vaibhav Parmar <vaibhav@nickelfox.com>
2023-04-12 10:46:44 +0530

    feat(hooks): add py files to check ambiguous constraints and commit messages

--- pre_commit_hooks/check_commit_message.py
+++ pre_commit_hooks/check_commit_message.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-def feat(message):
+def check(message):
     regex = r'^(feat|fix|docs|style|refactor|perf|test|chore)(\s(feat|fix|docs|style|refactor|perf|test|chore))*\([^\)]+\):\s.+$'
     match = re.match(regex, message)
     if match:
@@ -10,8 +10,10 @@ def feat(message):
         return False
 
 if __name__ == "__main__":
-    message = sys.argv[1]
-    if feat(message):
-        print("Message follows regex pattern!")
+    commit_message_file = sys.argv[1]
+    message = open(commit_message_file).read().strip()
+    if check(message):
+        print("Valid Commit message!")
     else:
-        print("Message does not follow regex pattern.")
+        print("Not a valid commit message")
+        print("Please follow git commit guidelines")
